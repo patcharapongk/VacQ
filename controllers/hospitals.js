b661eadea3b9a9fa92966ce4ea399058995d3a21
patcharapongk <6230362521@student.chula.ac.th>
2023-02-12 12:16:40 +0700

    Finished week5

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,34 +1,83 @@
+const Hospital = require('../models/HospitalModel');
 //@desc Get all hospitals
 //@route GET /api/v1/hospitals
 //@access Public
-exports.getHospitals = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Show all hospitals" });
+exports.getHospitals = async (req, res, next) => {
+    try {
+        const hospitals = await Hospital.find();
+
+        res.status(200).json({ success: true, count: hospitals.length, data: hospitals });
+    } catch (err) {
+        res.status(400).json({ success: false });
+
+    }
+
 }
 
 //@desc Get single hospital
 //@route GET /api/v1/hospitals/:id
 //@access Public
-exports.getHospital = (req, res, next) => {  
-    res.status(200).json({ success: true, msg: `Showing hospital ${req.params.id}` });
-}
+exports.getHospital = async (req, res, next) => {
+    try {
+        const hospital = await Hospital.findById(req.params.id);
+
+        if (!hospital) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: hospital });
+    }
+    catch (err) {
+        res.status(400).json({ success: false });
+    }
+};
 
 //@desc Create new hospital
 //@route POST /api/v1/hospitals
 //@access Private
-exports.createHospital = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Created new hospital" });
-}
+exports.createHospital = async (req, res, next) => {
+    const hospital = await Hospital.create(req.body);
+    console.log(hospital);
+    res.status(201).json({
+        success: true,
+        data: hospital
+    });
+    // res.status(200).json({ success: true, msg: "Created new hospital" });
+};
+
+
 
 //@desc Update hospital
 //@route PUT /api/v1/hospitals/:id
 //@access Private
-exports.updateHospital = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Updated hospital ${req.params.id}` });
-}
+exports.updateHospital = async (req, res, next) => {
+    try {
+        const hospital = await Hospital.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if (!hospital) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: hospital });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+
+} 
 
 //@desc Delete hospital
 //@route DELETE /api/v1/hospitals/:id
 //@access Private
-exports.deleteHospital = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Deleted hospital ${req.params.id}` });
+exports.deleteHospital = async (req, res, next) => {
+    try {
+        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        console.log(hospital);
+        if (!hospital)
+            res.status(400).json({ success: false });
+
+        res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
+
 }
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const dotenv = require('dotenv');
-
-// Route file
-const hospitals = require('./routes/hospitals');
+const dotenv = require('dotenv'); 
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route file
+const hospitals = require('./routes/hospitals');
+
+
 const app = express();
+app.use(express.json());
 
 // Mount routers
 app.use('/api/v1/hospitals', hospitals);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
