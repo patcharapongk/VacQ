d6b1834206298d0285bf7f4f0bd0e88a6e3f2bb8
patcharapongk <6230362521@student.chula.ac.th>
2023-02-23 12:19:33 +0700

    Week6, finished 08-16 authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -30,3 +30,14 @@ exports.protect = async (req, res, next) => {
         return res.status(401).json({ success: false, error: 'Not authorized to access this route'});
     }
 }
+
+exports.authorize = (...roles) => {
+
+    // return function
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ success: false, error: `User role ${req.user.role} is not authorized to access this route`});
+        }
+        next();
+    }
+}
